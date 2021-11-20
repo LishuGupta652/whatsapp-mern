2834bb195e7be08ff315ce66694750392e60615a
lishu <lishugupta652@gmail.com>
2021-11-20 23:42:51 +0530

    database added

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,26 @@
 // imports
-import express from express;
-
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 // App Config
-const app = express;
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(express.JSON())
+app.use(express.json());
 
 // DB config
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("Database Connencted");
+});
 
 // Api routes
-app.get('/', (req, res ) => {
-    res.status(200).send("hello")
-})
-
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
 
 // listen
-app.listen(PORT, () => { console.log(`app live on http://localhost:${PORT}`);})
+app.listen(PORT, () => {
+  console.log(`app live on http://localhost:${PORT}`);
+});
