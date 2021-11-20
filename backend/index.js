42be8668646eb6b6b6ced0de51be00a0ce242e14
lishu <lishugupta652@gmail.com>
2021-11-21 00:02:16 +0530

    message routes'

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import Message from "./dbMessage.js";
+cors();
 dotenv.config();
 // App Config
 const app = express();
@@ -18,6 +21,30 @@ mongoose.connect(process.env.MONGODB_URI, () => {
 // Api routes
 app.get("/", (req, res) => {
   res.status(200).send("hello");
+});
+
+app.get("/api/v1/message/", (req, res) => {
+  const dbMessage = req.body;
+
+  Message.find(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(` ${data}`);
+    }
+  });
+});
+
+app.post("/api/v1/message/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Message.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`New message create: ${data}`);
+    }
+  });
 });
 
 // listen
