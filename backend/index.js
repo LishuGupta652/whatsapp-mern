47d9a4208854d36eea3a740a48691fe6fd385195
lishu <lishugupta652@gmail.com>
2021-11-21 01:55:32 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import Message from "./dbMessage.js";
 import Pusher from "pusher";
 
-cors();
 dotenv.config();
 // App Config
 const app = express();
@@ -28,6 +27,7 @@ const pusher = new Pusher({
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Acess-Control-Allow-Origin", "*");
   res.setHeader("Acess-Control-Allow-Headers", "*");
